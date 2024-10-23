2f920ed0dc812de9ac9851c56498122f6ee12e31
Snekha2405 <220701282@rajalakshmi.edu.in>
2024-10-23 14:08:54 +0530

    Stage3

--- server.js
+++ server.js
@@ -1,11 +1,1 @@
-//updated
-const http1 = require('http');//calling the weserver protocol = http - inbuilt module
-const webserver=http1.createServer((req,res)=>{//create the web server
-    
-    res.end("hello chennai"); //response to the client
-});
-webserver.listen(2222,()=> //assign the port number
-{
-    
-    console.log("The server running under the port number: http://localhost:2222");//for inform to the user for running our app
-});
+//heloo
